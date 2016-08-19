6cf6e4993f376d6d06c458c173d0ba4e6f02412b
Phil Moreira <philm@propellerhead.ca>
2016-08-19 15:37:04 -0400

    new carousel splash page

--- components/SplashPage.js
+++ components/SplashPage.js
@@ -1,14 +1,24 @@
 'use strict';
 
 import React, { Component } from 'react';
-import {
+import { View, Text, StyleSheet } from 'react-native';
 
-  View,
-  Text,
-} from 'react-native';
+import Carousel from '../libs/carousel-hybrid';
 
+import Dimensions from 'Dimensions';
+let {width, height} = Dimensions.get('window');
 
 class SplashPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {size: {width: width, height: height}};
+    this._onLayoutDidChange = this._onLayoutDidChange.bind(this);
+  }
+
+  _onLayoutDidChange(e) {
+    let layout = e.nativeEvent.layout;
+    this.setState({size: {width: layout.width, height: layout.height}});
+  }
   componentWillMount() {
     var navigator = this.props.navigator;
       // setTimeout(() => {
@@ -19,11 +29,23 @@ class SplashPage extends Component {
     }
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: '#003399', alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{color: 'white', fontSize: 32,}}>Splash!</Text>
+      <View style={{flex: 1}} onLayout={this._onLayoutDidChange}>
+        <Carousel delay={500} style={this.state.size} autoplay={false} >
+            <View style={[styles.carousel_objs, {backgroundColor:'green'}, this.state.size]}><Text>Splash 1</Text></View>
+            <View style={[styles.carousel_objs, {backgroundColor:'red'}, this.state.size]}><Text>Splash 2</Text></View>
+            <View style={[styles.carousel_objs, {backgroundColor:'blue'}, this.state.size]}><Text>Splash 3</Text></View>
+        </Carousel>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  carousel_objs: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 
 module.exports = SplashPage;
